c6c48fd94dd026f5d1585768077dadf45967cad0
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-02-08 14:49:58 +0100

    minor: add type on logger

--- src/main/scala/it/unibo/Logger.scala
+++ src/main/scala/it/unibo/Logger.scala
@@ -1,7 +1,7 @@
 package it.unibo
 
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 
 object Logger {
-  def apply() = LoggerFactory.getLogger("")
+  def apply(): Logger = LoggerFactory.getLogger("")
 }
